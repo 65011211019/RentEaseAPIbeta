fc086b4adb3d3535572a60c763531e4fec60b310
65011211019 <138645306+65011211019@users.noreply.github.com>
2024-12-16 02:08:20 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./api/user');  // ใช้เส้นทางที่ถูกต้อง
 const cors = require('cors');  // เพิ่มการ import cors
 
-
-app.use(cors());
-
-// Import the database connection
-const db = require('./db');
-
 const app = express();
 const PORT = 5000;
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.json());
-app.use('/api', userRoutes);
 
+// เพิ่มเส้นทางที่ตอบกลับข้อความ "RentEaseAPI" เมื่อเข้าถึง / 
+app.get('/', (req, res) => {
+    res.send('RentEaseAPI');
+});
+
+// ใช้เส้นทาง API user
+app.use('/api', userRoutes);
 
 // Start Server
 app.listen(PORT, () => {
